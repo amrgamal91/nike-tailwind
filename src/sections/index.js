dd4daf39f15db71ff700848de2642bfa55d63ec6
Amr Gamal <ag.amrgamal91@gmail.com>
2023-10-05 12:52:48 +0300

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  CustomerReviews,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  Footer,
+};
